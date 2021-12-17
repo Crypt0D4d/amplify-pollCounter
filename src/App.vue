6b77035e3edce5ef0c24665d94128fb365744e81
DadOps <dadops@dadops.cloud>
2021-12-16 23:26:51 -0800

    Change absinthe image size in vue card

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,13 @@
 
     <b-row align-h="center" class="mt-5">
       <b-card-group deck>
-        <b-card bg-variant="dark" img-src="https://i.ibb.co/x8m49M4/absinthe.png" text-variant="white" header="Vote @iamabsinthe" class="text-center" footer-tag="footer">
+        <b-card bg-variant="dark" img-src="https://i.ibb.co/x8m49M4/absinthe.png" img-height="400" img-width="400" text-variant="white" header="Vote @iamabsinthe" class="text-center" footer-tag="footer">
           <b-card-text>Vote Pris</b-card-text>
           <b-button size="lg" variant="primary"  @click="vote('yes')">Vote Pris</b-button>
           <em slot="footer">{{ votesYes }} voted</em>
         </b-card>
 
-        <b-card bg-variant="danger" img-src="https://i.ibb.co/3Wp85b2/rei.png" height="580" text-variant="white" header="Vote @reincanno" class="text-center" footer-tag="footer">
+        <b-card bg-variant="danger" img-src="https://i.ibb.co/3Wp85b2/rei.png" text-variant="white" header="Vote @reincanno" class="text-center" footer-tag="footer">
           <b-card-text>Vote Rei</b-card-text>
           <b-button size="lg" variant="primary" @click="vote('no')">Vote Rei</b-button>
           <em slot="footer">{{ votesNo }} voted</em>
