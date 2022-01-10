73c62134576777c92497b0e9ae389142010b1f37
DadOps <dadops@dadops.cloud>
2022-01-09 22:13:56 -0800

    Change to clementine

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
-  <div id="app" :style="{'background-image':'url(https://www.nanopass.io/assets/images/cyberpunk_hq.png)'}" class="hello" >
+  <div id="app" :style="{'background-image':'url(https://cdn.discordapp.com/attachments/917544129691541575/925826655866462208/Clementine_SOCIAL_AH_SW_01.jpg)'}" class="hello" >
 
-    <h1 style="color:white;">Welcome to the Nanopass clout war!</h1>
-    <h4 style="color:white;">Click to decide who has more clout!</h4>
+    <h1 style="color:white;">Welcome to the Clementine faction war!</h1>
+    <h4 style="color:white;">Click to decide which faction is best!</h4>
     <h4 style="color:white;">You can vote as many times as you like!</h4>
 
     <b-row align-h="center" class="mt-5">
+      <!-- hollow -->
       <b-card-group deck>
+        <b-card bg-variant="dark" img-src="https://cdn.discordapp.com/attachments/917544129691541575/928826387375222804/Clementine_Golem_looped.mp4" img-height="500" img-width="400" text-variant="white" header="Vote @iamabsinthe" class="text-center" footer-tag="footer">
+          <b-card-text>Vote Dreamers</b-card-text>
+          <b-button size="lg" variant="primary"  @click="vote('dreamer')">Vote Dreamer</b-button>
+          <em slot="footer">{{ votesDreamer }} voted</em>
+        </b-card>
+
+        <!-- kami -->
+        <b-card bg-variant="danger" img-src="https://i.ibb.co/3Wp85b2/rei.png" text-variant="white" header="Vote @reincannon" class="text-center" footer-tag="footer">
+          <b-card-text>Vote Rei</b-card-text>
+          <b-button size="lg" variant="primary" @click="vote('no')">Vote Rei</b-button>
+          <em slot="footer">{{ votesNo }} voted</em>
+        </b-card>
+
+        <!-- idk -->
         <b-card bg-variant="dark" img-src="https://i.ibb.co/x8m49M4/absinthe.png" img-height="500" img-width="400" text-variant="white" header="Vote @iamabsinthe" class="text-center" footer-tag="footer">
           <b-card-text>Vote Pris</b-card-text>
           <b-button size="lg" variant="primary"  @click="vote('yes')">Vote Pris</b-button>
           <em slot="footer">{{ votesYes }} voted</em>
         </b-card>
 
-        <b-card bg-variant="danger" img-src="https://i.ibb.co/3Wp85b2/rei.png" text-variant="white" header="Vote @reincannon" class="text-center" footer-tag="footer">
-          <b-card-text>Vote Rei</b-card-text>
-          <b-button size="lg" variant="primary" @click="vote('no')">Vote Rei</b-button>
-          <em slot="footer">{{ votesNo }} voted</em>
-        </b-card>
+
       </b-card-group>
     </b-row>
     <b-row align-h="center" class="mt-5">
-      <p style="color:white;">Questions? Ask Crypt0D4d <a href="https://twitter.com/Crypt0d4d">@Crypt0D4d</a>.</p>
+      <p style="color:white;">Questions? Ask AlphaIndustries <a href="https://twitter.com/AlphaIndustry">@AlphaIndustry</a>.</p>
     </b-row>
   </div>  
 </template>
@@ -46,13 +57,13 @@ export default {
         }
       }
       const response = await API.post(this.apiName, '/votes', init)
-      if (vote === 'yes') this.votesYes = response.data.Attributes.votesYes
+      if (vote === 'dreamer') this.votesDreamer = response.data.Attributes.votesDreamer
       if (vote === 'no') this.votesNo = response.data.Attributes.votesNo
     },
     updateVotes: async function () {
       const response = await API.get(this.apiName, '/votes/poll-001')
       this.votesNo = response[0].votesNo
-      this.votesYes = response[0].votesYes    
+      this.votesYes = response[0].votesDreamer
     }
   },
   created () {
